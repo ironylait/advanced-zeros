bd10a8048599b2f719bfd90b0b1c5a9c03a21d87
ironylait <ironylait@mail.ru>
2019-03-19 20:50:50 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  if (1 > number || number > 10000000) throw new Error({error: 'Eror'});
+  var b; 
+  var a = number; 
+  var counter=0;
+  
+ 
+ function factorial(numb) {
+  var fact=1;
+     for (var i = 2; i <= numb; i++)
+         fact = fact * i;
+     return fact; 
+ }                                 //находим факториал
+   
+ b = factorial(a).toString(base); //перевод системы счисления 
+ b = +b;                          //преобразвание в число
+ do {
+    counter++;
+    b = b /10; 
+ } while (b % 10 == 0);        
+ 
+   
+ return counter;   
 }
