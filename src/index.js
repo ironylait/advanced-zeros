0cba7b82f6344908efa9e66913429a738814edd1
ironylait <ironylait@mail.ru>
2019-04-04 16:28:54 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-    let zero = number;
+let zero = number;
+var arr = [];
 
-                                
+
     for (let i = 2; i <= base; i++) {
-      if (base % i === 0) {                 //первое число, которое делиться без остатки(i)
-        let X = 0;
+      if (base % i === 0) {                     //первое число, которое делиться без остатка(i) 
+        let powerBase = 0;
         while (base % i === 0) {           
            base /= i;     
-           X++;                             //степень числа, котороеделиться без остатка (i) 
+           powerBase++;                         //степень числа(i) 
         }
-        let c = 0;
-        let z = Math.floor(number / i);    //степень числа (i) в 10 системе 
+        let powerNumber = 0;                    //степень числа (i) в number
+        let z = Math.floor(number / i);    
         while (z > 0) {          
-          c += z;     
+          powerNumber += z;     
           z = Math.floor(z / i);  
         }
-        zero = Math.floor(c / X);
+        zero = Math.floor(powerNumber / powerBase);
+        arr.push(zero);
       }
     }
-    return zero;
+const min = Math.min(...arr);
+return min;
 }
 
-/* https://brilliant.org/wiki/trailing-number-of-zeros/
+/* 
+   https://brilliant.org/wiki/trailing-number-of-zeros/
    https://www.geeksforgeeks.org/number-of-trailing-zeroes-in-base-b-representation-of-n/
-   https://www.quora.com/How-do-I-find-the-number-of-trailing-zeroes-of-N-factorial-in-Base-B */
+   https://www.quora.com/How-do-I-find-the-number-of-trailing-zeroes-of-N-factorial-in-Base-B
+*/
 
-/*{
+
+/* 
+  //First try
+  {
   if (1 > number || number > 10000000) throw new Error({error: 'Eror'});
   var b; 
   var a = number; 
@@ -48,4 +56,5 @@ module.exports = function getZerosCount(number, base) {
  
    
  return counter;   
-} */ 
+} 
+*/ 
